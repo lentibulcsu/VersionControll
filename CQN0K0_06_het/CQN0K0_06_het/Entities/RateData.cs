1c9038979a62f682cfddc1eb24830798e951b04c
Bulcsú András Lenti <bulcsu.lenti@stud.uni-corvinus.hu>
2022-10-21 14:19:55 +0200

    cigany kommit

---
+++ CQN0K0_06_het/CQN0K0_06_het/Entities/RateData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQN0K0_06_het.Entities
+{
+    class RateData
+    {
+        public DateTime Date { get; set; }
+        public string Currency { get; set; }
+        public decimal Value { get; set; }
+    }
+}
--- CQN0K0_06_het/CQN0K0_06_het/Form1.cs
+++ CQN0K0_06_het/CQN0K0_06_het/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CQN0K0_06_het.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,37 @@ namespace CQN0K0_06_het
 {
     public partial class Form1 : Form
     {
+        BindingList<Entities.RateData> Rates = new BindingList<Entities.RateData>();
+
         public Form1()
         {
             InitializeComponent();
+
+            ElsoFuggveny();
+
+            dataGridView1.DataSource = Rates;
+        }
+
+        private void ElsoFuggveny()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+        }
+
+        private void MasodikFuggveny()
+        {
+
         }
     }
 }
