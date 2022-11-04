0131d2c96b30c016073b77d9acbcd36844f872eb
Bulcsú András Lenti <bulcsu.lenti@stud.uni-corvinus.hu>
2022-11-04 15:40:06 +0100

    Futoszalag

--- gyak_3_cqn0k0/gyak_3_cqn0k0/Form1.cs
+++ gyak_3_cqn0k0/gyak_3_cqn0k0/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using gyak_3_cqn0k0.Entities1;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,14 +13,53 @@ namespace gyak_3_cqn0k0
 {
     public partial class Form1 : Form
     {
+        private List<Ball> _balls = new List<Ball>();
+
+        private BallFactory _factory;
+        private BallFactory Factory //publicrol at lett irva privatera
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+
+
         public Form1()
         {
             InitializeComponent();
+            Factory = new BallFactory();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var ball = Factory.CreateNew();
+            _balls.Add(ball);
+            ball.Left = -ball.Width;
+            mainPanel.Controls.Add(ball);
+
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var maxPosition = 0;
+
+            foreach (var ball in _balls)
+            {
+                ball.MoveBall();
+                if (ball.Left > maxPosition)
+                    maxPosition = ball.Left;
+            }
+
+            if (maxPosition>=1000)
+            {
+                var elso = _balls[0];
+                mainPanel.Controls.Remove(elso);
+                _balls.Remove(elso);
+            }
         }
     }
 }
