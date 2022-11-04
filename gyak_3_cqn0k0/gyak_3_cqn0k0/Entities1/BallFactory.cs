c145f92e1d36c5489575e869117dd836b10bd35d
Bulcsú András Lenti <bulcsu.lenti@stud.uni-corvinus.hu>
2022-11-04 15:01:21 +0100

    Labdagyar letrehozasa

---
+++ gyak_3_cqn0k0/gyak_3_cqn0k0/Entities1/BallFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak_3_cqn0k0.Entities1
+{
+    class BallFactory
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
