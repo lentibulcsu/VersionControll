ee8a8a40d9834c51abf4c75911e94a96552e1f66
Bulcsú András Lenti <bulcsu.lenti@stud.uni-corvinus.hu>
2022-11-25 14:49:28 +0100

    masodik2

--- UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -1,12 +1,46 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitTestExample.Controllers;
 
 namespace UnitTestExample.Test
 {
     public class AccountControllerTestFixture
     {
+        [
+            Test,
+            TestCase("abcd1234", false),
+            TestCase("irf@uni-corvinus", false),
+            TestCase("irf.uni-corvinus.hu", false),
+            TestCase("irf@uni-corvinus.hu", true)
+        ]
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.ValidateEmail(email);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [
+            Test,
+            TestCase("asbdAbsd", false),
+            TestCase("ASDAS3RHS", false),
+            TestCase("ahsds3hegw", false),
+            TestCase("dasd3A", false),
+            TestCase("bulC5U17s", true)
+        ]
+
+        public void TestValidatePassword()
+        {
+
+        }
     }
 }
