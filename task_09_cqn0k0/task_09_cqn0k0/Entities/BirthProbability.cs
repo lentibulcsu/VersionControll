67996e39ae9f38b963629fea12134754de012911
Bulcsú András Lenti <bulcsu.lenti@stud.uni-corvinus.hu>
2022-12-02 11:03:28 +0100

    Osztalyok letrehozasa

---
+++ task_09_cqn0k0/task_09_cqn0k0/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_09_cqn0k0.Entities
+{
+    class BirthProbability
+    {
+        public int Age { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double Probability { get; set; }
+    }
+}
---
+++ task_09_cqn0k0/task_09_cqn0k0/Entities/DeathProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_09_cqn0k0.Entities
+{
+    class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+        public double Probability { get; set; }
+
+    }
+}
